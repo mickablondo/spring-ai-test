b676d3c6cdd39e995a4a7ac42f8c202e66558459
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2024-12-29 14:38:38 +0100

    Amélioration du style et gestion des erreurs

---
+++ src/main/java/dev/mikablondo/springaitest/AppShell.java
@@ -0,0 +1,9 @@
+package dev.mikablondo.springaitest;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@Theme(themeClass = Lumo.class, variant = Lumo.DARK)
+public class AppShell implements AppShellConfigurator {
+}
--- src/main/java/dev/mikablondo/springaitest/MainView.java
+++ src/main/java/dev/mikablondo/springaitest/MainView.java
@@ -13,11 +13,21 @@ public class MainView extends VerticalLayout {
 
     public MainView(ChatClient chatClient) {
         var question = new TextField();
+        question.addClassName("question-field");
+
         var ask = new Button("Pose ta question");
+        ask.addClassName("ask-button");
+
         var answer = new Paragraph();
+        answer.addClassName("answer-paragraph");
 
         ask.addClickListener(event -> {
-           answer.setText(chatClient.call(question.getValue()));
+            try {
+                answer.setText(chatClient.call(question.getValue()));
+            } catch (Exception e) {
+                answer.setText("Une erreur est survenue lors de la pose de la question.");
+            }
+            question.clear();
         });
 
         add(
